1d6178715c5cd8f470a97c366570e22ddc780e49
Knut Olav Platou Bøhmer <bohmer@gmail.com>
2013-02-06 14:58:38 +0100

    extending ftop.sh from a one-liner to making it a bit more usable

--- bashrc
+++ bashrc
@@ -5,9 +5,19 @@
 
 
 alias lsd="ls --color=always -h --group-directories-first"
+alias ls="ls --color=always -Fh"
 
-highlight () {  egrep --color=always "$1|" "$2" ; }
+highlight  () { 
+    egrep --color=always "$1|" "$2" ; 
+}
 
+interfaces () { 
+    ip link show | awk '/^[0-9]*:/ {sub(":","",$2); print $2}';
+}
 
-
-
+ipaddresses () {
+    for iface in $( interfaces ) ; do 
+	ip address show $iface | awk -v iface=$iface \
+	    '/inet / {printf "%7s: %s\n", iface , $2}';
+    done
+}
--- ftop.sh
+++ ftop.sh
@@ -1,16 +1,60 @@
-#!/bin/bash
+#!/bin/bash 
 
-PID=$(pidof tar);
+PROGRAM=tar
+
+while getopts "p:c:dnh?" opt; do
+    case $opt in
+	d)
+	    DEBUG=1
+	    ;;
+	p)
+	    PID=$OPTARG
+	    ;;
+	c)
+	    PROGRAM=$OPTARG
+	    ;;
+	n)
+	    DRYRUN=yes
+	    ;;
+	h|\?)
+	    echo -e "Usage $0 [-dpcn]\n" >&2
+	    exit 0;
+	    ;;
+  esac
+done
+
 if [ -z "$PID" ]; then
-  echo Error: tar is not running
-  exit 0
+    PID=$(pidof -s ${PROGRAM:-tar});
+    if [ -z "$PID" ]; then
+	echo Error: $PROGRAM is not running
+	exit 0
+    fi;
 fi
 
-FDINFO=/proc/$PID/fdinfo/3;
-SIZE=$(stat -L --printf "%s" /proc/$PID/fd/3);
-FILE=$(readlink /proc/$PID/fd/3)
+for each in $(find /proc/$PID/fd -xtype f -printf "%f ") ; do
+    # TODO, maybe show all files used
+    FDESK=${FDESK:-$each}  
+done
 
-while [ -f $FDINFO ] ;  do
+FDINFO=/proc/$PID/fdinfo/$FDESK;
+SIZE=$(stat -L --printf "%s" /proc/$PID/fd/$FDESK);  
+FILE=$(readlink /proc/$PID/fd/$FDESK)
+
+if [ "$DEBUG" == 1 ]; then 
+    echo FDESK: $FDESK 
+    echo FDINFO: $FDINFO
+    echo SIZE: $SIZE
+    echo FILE: $FILE
+    echo PID: $PID
+    echo PROGRAM: $PROGRAM
+    echo DRYRUN: $DRYRUN
+fi
+
+if [ "$DRYRUN" = yes ]; then
+    exit 0
+fi
+
+while [ -f "$FDINFO" ] ;  do
     awk  -v size=$SIZE -v file=$FILE \
 	'/pos:/ { printf "%s:   %3.2f %%  (%s/%s) bytes\r" ,file ,($2/size*100),$2,size;}' $FDINFO;
     sleep .5;
