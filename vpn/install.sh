c21bd9f066fc4b0c3ecc4ee0aa5a8fd45ae724ff
root <root@localhost.localdomain>
2013-02-10 00:03:34 +0100

    Bugfixing restart function Adding more to the todo list

--- vpn/install.sh
+++ vpn/install.sh
@@ -16,14 +16,27 @@ else
     echo initscript exsist. Not touching
 fi;
 
-exit 0
 
-echo TODO:
-read -p "IP to vpnserver: " IP
-echo $IP     vpnserver >> /etc/hosts
+echo "# TODO:"
+read -p "vpnserver name (default: vpnserver): " VPNSERVER
+
+if [ -z "$VPNSERVER" ]; then 
+    VPNSERVER=vpnserver
+fi
+
+IP=$(getent ahosts $VPNSERVER | grep STREAM| awk '{print $1}')
+
+if [ -z "$IP" ]; then 
+    read -p "IP to vpnserver: " IP
+    echo $IP     vpnserver >> /etc/hosts
+fi
 
 echo ssh-keygen -t dsa -N "" -f ~/.ssh/vpn-ssh-key
 echo ssh-copy-id -i ~/.ssh/vpn-ssh-key vpn@vpnserver
+
+echo "# Do this: OR"
+SSH_KEY=$(cat ~/.ssh/vpn-ssh-key)
+echo command="sudo \${SSH_ORIGINAL_COMMAND#* }" $SSH_KEY
 
 echo "### Add the following to the vpnservers sudofile:"
 echo Cmnd_Alias VPN=/usr/sbin/pppd
