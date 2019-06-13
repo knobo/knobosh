43aae06c9acfa67762ebc81234631bc20b474776
Knut Olav Bøhmer <bohmer@gmail.com>
2019-06-13 11:21:21 +0200

    gclone - Only use ssh clone for my own repos

--- gclone.sh
+++ gclone.sh
@@ -4,12 +4,13 @@ cd ~/prog
 
 if [[ $1 =~ / ]]; then
     echo cloning $1
+    git clone https://github.com/${1}.git
 else
     echo cloning knobo/$1
     USER=knobo/
+    git clone git@github.com:${USER}${1}.git;
 fi
 
-git clone git@github.com:${USER}${1}.git;
 
 
 
